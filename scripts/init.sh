7bfded18244aecfb481888e61931f1264515ce3f
carlos <carlos@example.com>
2020-06-14 15:39:00 +1000

    Fixing to 644a160 to get mysql to work properly3.

--- scripts/init.sh
+++ scripts/init.sh
@@ -321,7 +321,7 @@ else
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -y install mysql-server
-#fi
+fi
 
 fi
 
