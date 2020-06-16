98aaaf47b2130f9e31ac92f28c87867ef5c02341
carlos <carlos@example.com>
2020-06-16 19:49:41 +1000

    Creating php7.3 version

--- scripts/init.sh
+++ scripts/init.sh
@@ -308,8 +308,8 @@ sudo apt-get -qq update -y && sudo apt-get -qq upgrade -y
 ## Install AMP
 echo -e "$Cyan \n Installing Apache2 etc $Color_Off"
 # php-gettext not installing on ubuntu 20
-sudo apt-get -qq install apache2 php libapache2-mod-php php-mysql php-gettext curl php-cli php-gd php-mbstring php-xml php-curl php-bz2 php-zip git unzip php-xdebug -y
-#sudo apt-get -qq install apache2 php7.3 libapache2-mod-php php7.3-mysql php7.3-gettext curl php7.3-cli php7.3-gd php7.3-mbstring php7.3-xml php7.3-curl php7.3-bz2 php7.3-zip git unzip php7.3-xdebug -y
+#sudo apt-get -qq install apache2 php libapache2-mod-php php-mysql php-gettext curl php-cli php-gd php-mbstring php-xml php-curl php-bz2 php-zip git unzip php-xdebug -y
+sudo apt-get -qq install apache2 php7.3 libapache2-mod-php php7.3-mysql php7.3-gettext curl php7.3-cli php7.3-gd php7.3-mbstring php7.3-xml php7.3-curl php7.3-bz2 php7.3-zip git unzip php7.3-xdebug -y
 
 fi
 
